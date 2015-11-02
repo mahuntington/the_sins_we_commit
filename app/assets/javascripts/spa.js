1d4392e99a2df647777f6f3755a37dcf56930522
Matt Huntington <matt.huntington@gmail.com>
2015-11-02 16:39:21 -0500

    add a transgression

--- app/assets/javascripts/spa.js
+++ app/assets/javascripts/spa.js
@@ -12,4 +12,19 @@ app.controller('TransgressionsController', ['$http', function($http){
   $http.get('/transgressions').success(function(data){
     controller.transgressions = data.transgressions;
   });
+
+  this.createTransgression = function(){
+    var csrf = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    $http.post('/transgressions', {
+      authenticity_token: csrf,
+      transgression: {
+        sin_type: controller.newTransgressionSinType,
+        description: controller.newTransgressionDescription
+      }
+    }).success(function(data){
+      $http.get('/transgressions').success(function(data){
+        controller.transgressions = data.transgressions;
+      });
+    });
+  }
 }]);
