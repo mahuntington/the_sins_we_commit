0ed4ee13e73083c9a1bcd7ad3dd2520fa85690d4
Matt Huntington <matt.huntington@gmail.com>
2015-11-03 10:52:13 -0500

    header controller gets current user and displays it

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,5 +1,9 @@
 var app = angular.module('SinsApp', []);
 
-app.controller('HeaderController', function(){
-  this.foo = 'bar'
-});
+app.controller('HeaderController', ['$http', function($http){
+  var controller = this;
+  $http.get('/session').success(function(data){
+    controller.current_user = data.current_user;
+    console.log(controller.current_user);
+  })
+}]);
